e8c846233464d787c0b4452887f08db749df2676
Abdul Ripon <mdabdulripon@gmail.com>
2021-12-03 16:43:29 -0600

    feat(add basket summary page)

--- src/features/basket/BasketPage.tsx
+++ src/features/basket/BasketPage.tsx
@@ -1,9 +1,10 @@
 import { Add, Delete, Remove } from "@mui/icons-material";
 import { LoadingButton } from "@mui/lab";
-import { Box, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import { Box, Grid, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
 import { useState } from "react";
 import agent from "../../app/api/agent";
 import { useStoreContext } from "../../app/context/StoreContext";
+import BasketSummary from "./BasketSummary";
 
 export default function BasketPage() {
 
@@ -34,52 +35,62 @@ export default function BasketPage() {
 	if (!basket || basket.items.length === 0) return <Typography>Your basket is empty</Typography>
 	
 	return (
-		<TableContainer component={Paper}>
-			<Table sx={{ minWidth: 650 }} aria-label="simple table">
-				<TableHead>
-					<TableRow>
-					<TableCell>Product</TableCell>
-					<TableCell align="right">Price</TableCell>
-					<TableCell align="right">Quantity</TableCell>
-					<TableCell align="right">Subtitle</TableCell>
-					<TableCell align="right"></TableCell>
-					</TableRow>
-				</TableHead>
-				<TableBody>
-					{basket.items.map((item) => (
-					<TableRow
-						key={item.productId}
-						sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-					>
-						<TableCell component="th" scope="row">
-							<Box display='flex' alignItems='center'>
-								<img src={item.pictureUrl} alt={item.name} style={{ height: 50, marginRight: 20}} />
-								<span>{ item.name }</span>
-							</Box>
-						</TableCell>
-						<TableCell align="right">{(item.price).toFixed(2)}</TableCell>
-						<TableCell align="right">
-							<LoadingButton color='error' loading={status.loading && status.name === `rem-${item.productId}`}
-								onClick={() => handleRemoveItem(item.productId, 1, `rem-${item.productId}`)}>
-								<Remove />
-							</LoadingButton>
-							<span>{item.quantity}</span>
-							<LoadingButton color='primary' loading={status.loading && status.name === `add-${item.productId}`} 
-								onClick={() => handleAddItem(item.productId, `add-${item.productId}`)}>
-								<Add />
-							</LoadingButton>
-						</TableCell>
-						<TableCell align="right">{(item.price * item.quantity).toFixed(2)}</TableCell>
-						<TableCell align="right">
-							<LoadingButton color='error' loading={status.loading && status.name === `del-${item.productId}`} 
-								onClick={() => handleRemoveItem(item.productId, item.quantity, `del-${item.productId}`)}>
-								<Delete />
-							</LoadingButton>
-						</TableCell>
-					</TableRow>
-					))}
-				</TableBody>
-			</Table>
-		</TableContainer>
+		<>
+			<TableContainer component={Paper}>
+				<Table sx={{ minWidth: 650 }} aria-label="simple table">
+					<TableHead>
+						<TableRow>
+						<TableCell>Product</TableCell>
+						<TableCell align="right">Price</TableCell>
+						<TableCell align="right">Quantity</TableCell>
+						<TableCell align="right">Subtitle</TableCell>
+						<TableCell align="right"></TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{basket.items.map((item) => (
+						<TableRow
+							key={item.productId}
+							sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+						>
+							<TableCell component="th" scope="row">
+								<Box display='flex' alignItems='center'>
+									<img src={item.pictureUrl} alt={item.name} style={{ height: 50, marginRight: 20}} />
+									<span>{ item.name }</span>
+								</Box>
+							</TableCell>
+							<TableCell align="right">{(item.price).toFixed(2)}</TableCell>
+							<TableCell align="right">
+								<LoadingButton color='error' loading={status.loading && status.name === `rem-${item.productId}`}
+									onClick={() => handleRemoveItem(item.productId, 1, `rem-${item.productId}`)}>
+									<Remove />
+								</LoadingButton>
+								<span>{item.quantity}</span>
+								<LoadingButton color='primary' loading={status.loading && status.name === `add-${item.productId}`} 
+									onClick={() => handleAddItem(item.productId, `add-${item.productId}`)}>
+									<Add />
+								</LoadingButton>
+							</TableCell>
+							<TableCell align="right">{(item.price * item.quantity).toFixed(2)}</TableCell>
+							<TableCell align="right">
+								<LoadingButton color='error' loading={status.loading && status.name === `del-${item.productId}`} 
+									onClick={() => handleRemoveItem(item.productId, item.quantity, `del-${item.productId}`)}>
+									<Delete />
+								</LoadingButton>
+							</TableCell>
+						</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			</TableContainer>
+
+			<Grid container>
+				<Grid item xs={6}></Grid>
+				<Grid item xs={6}>
+					<BasketSummary />
+				</Grid>
+			</Grid>
+		</>
+		
 	)
 } 
---
+++ src/features/basket/BasketSummary.tsx
@@ -0,0 +1,39 @@
+import { TableContainer, Paper, Table, TableBody, TableRow, TableCell, Typography } from "@mui/material";
+import { useStoreContext } from "../../app/context/StoreContext";
+
+export default function BasketSummary() {
+    const { basket } = useStoreContext();
+    console.log(basket);
+    const subtotal = basket?.items.reduce((sum, item) => sum + (item.quantity * item.price), 0) || 0;
+    console.log("subtotal", subtotal);
+    const deliveryFee = subtotal > 100 ? 0 : 5;
+    
+
+    return (
+        <>
+            <TableContainer component={Paper} variant={'outlined'}>
+                <Table>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell colSpan={2}>Subtotal</TableCell>
+                            <TableCell align="right">{subtotal}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell colSpan={2}>Delivery fee*</TableCell>
+                            <TableCell align="right">{deliveryFee}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell colSpan={2}>Total</TableCell>
+                            <TableCell align="right">{subtotal + deliveryFee}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>
+                                <span style={{fontStyle: 'italic'}}>*Orders over $100 qualify for free delivery</span>
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </>
+    )
+}
